7c9c270e80837244ebace387cf6f3334b057032f
Ved Sharma <ved.sharma@sigmainfo.net>
2025-02-03 01:05:25 +0530

    feat: user model completed

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', async (req, res, next) => {
     res.send('Hello World!');
