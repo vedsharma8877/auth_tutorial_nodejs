c6289e89c11c858f78d8ffbb88c669c700ae3b3f
Ved Sharma <ved.sharma@sigmainfo.net>
2025-02-03 02:00:06 +0530

    feat: login completed

--- models/user.model.js
+++ models/user.model.js
@@ -26,5 +26,13 @@ userSchema.pre('save', async function (next) {
     }
 });
 
+userSchema.methods.isValidPassword = async function (password) {
+    try {
+        return await bcrypt.compare(password, this.password);
+    } catch (error) {
+        throw error;
+    }
+}
+
 const User = mongoose.model('User', userSchema);
 module.exports = User;
