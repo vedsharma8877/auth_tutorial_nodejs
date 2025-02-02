82d6c02e987a28dbe50ce6581fe3ab5321c8564f
Ved Sharma <ved.sharma@sigmainfo.net>
2025-02-03 00:24:43 +0530

    feat: started auth in nodejs

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+const createError = require('http-errors');
+require('dotenv').config();
+
+const app = express();
+
+app.get('/', async (req, res, next) => {
+    res.send('Hello World!');
+});
+
+app.use(async (req, res, next) => {
+    next(createError.NotFound());
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message,
+        },
+    });
+}    
+)
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
