bd3ba1ec3029045ce423219472ac30ea99cf37c1
Ved Sharma <ved.sharma@sigmainfo.net>
2025-02-03 01:16:23 +0530

    feat: validation with joi

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,18 +2,19 @@ const express = require('express');
 const router = express.Router();
 const createError = require('http-errors')
 const User = require('../models/user.model')
+const { authSchema } = require('../helpers/validation_schema');
 
 router.post('/register', async (req, res, next) => {
     console.log(req.body);
     try {
-        const { email, password } = req.body;
-        if (!email || !password) throw createError.BadRequest()
-        const doesExist = await User.findOne({ email: email })
-        if (doesExist) throw createError.Conflict(`${email} is already been registered.`)
-        const user = new User({ email, password })
+        const result = await authSchema.validateAsync(req.body)
+        const doesExist = await User.findOne({ email: result.email })
+        if (doesExist) throw createError.Conflict(`${result.email} is already been registered.`)
+        const user = new User(result)
         const savedUser = await user.save()
         res.send(savedUser)
     } catch (error) {
+        if (error.isJoi === true) error.status = 422
         next(error)
     }
 });
